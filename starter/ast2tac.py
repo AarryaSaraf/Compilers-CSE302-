5aa024ba3be21351738838f2c5794c1697df3e0d
Jonas Treplin <jonas@treplin.de>
2023-09-08 14:55:25 +0200

    add bmm

--- starter/ast2tac.py
+++ starter/ast2tac.py
@@ -1,16 +1,23 @@
 from lib.tac import var_mapping, serialize, pretty_print
 from lib.tmm import tmm
+from lib.bmm import bmm
 from lib.ast import deserialize
 import json
 import sys
 from typing import List
 
 
-def main(file):
+def main(file, method="tmm"):
     with open(file) as fp:
         js = json.load(fp)
     statements = deserialize(js)
     var_to_tmp = var_mapping(statements)
-    return pretty_print(tmm(statements, var_to_tmp))
+    if method=="tmm":
+        code = tmm(statements, var_to_tmp)
+    else:
+        code = bmm(statements, var_to_tmp)
+    return pretty_print(code)
+
 if __name__ == "__main__":
-    print(main(sys.argv[1]))
+    method = "bmm" if "--bmm" in sys.argv else "tmm"
+    print(main(sys.argv[1], method=method))
---
+++ starter/lib/bmm.py
@@ -0,0 +1,39 @@
+from .ast import *
+from .tac import *
+from typing import Dict, List, Tuple
+
+def bmm(statments: List[Statement], var_to_tmp: Dict[str, TACTemp]) -> List[TACOp]:
+    code = []
+    for stmt in statments:
+        match stmt: 
+            case StatementAssign(var, expr): 
+                res, expr_code  = bmm_code(expr, var_to_tmp)
+                code += expr_code + [TACOp("copy", [res], var_to_tmp[var])]
+            case StatementEval(expr): 
+                res, expr_code = bmm_code(expr, var_to_tmp)
+                code += expr_code
+            case StatementDecl(name): 
+                code += [TACOp("const", [0], var_to_tmp[name])]
+    return code
+
+def bmm_code(expr: Expression, var_to_tmp: Dict[str, TACTemp]) -> Tuple[TACTemp, TACOp]:
+    match expr:
+        case ExpressionVar(name): 
+            return var_to_tmp[name], []
+        case ExpressionInt(val):
+            result = fresh_temp()
+            return result, [TACOp("const", [val], result)]
+        case ExpressionBinOp(operator, left, right):
+            left_tmp, left_ops = bmm_code(left, var_to_tmp)
+            right_tmp, right_ops = bmm_code(right, var_to_tmp)
+            result = fresh_temp()
+            return result, left_ops + right_ops + [TACOp(OPERATOR_TO_OPCODE[operator], [left_tmp, right_tmp], result)]
+        case ExpressionUniOp(operator, arg):
+            result = fresh_temp()
+            arg_tmp, arg_op = bmm_code(arg, var_to_tmp)
+            return result, arg_op + [TACOp(OPERATOR_TO_OPCODE[operator], [arg_tmp], result)]
+        case ExpressionCall("print", [arg]):
+            arg_tmp, arg_op = bmm_code(arg, var_to_tmp)
+            return None, arg_op + [TACOp("print", [arg_tmp], None)]
+        case x:
+            raise ValueError(f"cant translate {x}")
