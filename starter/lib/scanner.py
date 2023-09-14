0fdfdcdc23f4ed8b6777f1cdb996415015ab5f1c
Jonas Treplin <jonas@treplin.de>
2023-09-14 14:32:12 +0200

    add scanner

---
+++ starter/lib/scanner.py
@@ -0,0 +1,91 @@
+import ply.lex as lex
+
+reserved = {"def": "DEF", "var": "VAR", "print": "PRINT", "int": "INT"}
+tokens = (
+    "IDENT",
+    "NUMBER",
+    "MAIN",
+    "LPAREN",
+    "RPAREN",
+    "VAR",
+    "EQUALS",
+    "COLON",
+    "SEMICOLON",
+    "PRINT",
+    "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
+    "MOD",
+    "AND",
+    "OR",
+    "XOR",
+    "LSHIFT",
+    "RSHIFT",
+    "TILDE",
+    "COMMENT",
+    "LBRACE",
+    "RBRACE",
+    "INT",
+    "DEF"
+)
+t_ignore = r" "
+
+def t_IDENT(t):
+    r"[A-Za-z][A-Za-z0-9_]*"
+    t.type = reserved.get(t.value, "IDENT")
+    return t
+    
+t_PLUS = r"\+"
+t_MINUS = r"\-"
+t_TIMES = r"\*"
+t_DIVIDE = r"\/"
+t_MOD = r"\%"
+t_AND = r"\&"
+t_OR = r"\|"
+t_XOR = r"\^"
+t_LSHIFT = r"<<"
+t_RSHIFT = r">>"
+t_TILDE = r"\~"
+t_LBRACE = r"{"
+t_RBRACE = r"}"
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_EQUALS = r"="
+t_COLON = r"\:"
+t_SEMICOLON = r";"
+
+
+
+
+
+def t_COMMENT(t):
+    r"//.*"
+    pass
+
+def t_NUMBER(t):
+    r"0|[1-9][0-9]*"
+    t.value = int(t.value)
+    return t
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+lexer = lex.lex()
+
+if __name__ == "__main__":
+    import sys
+    with  open(sys.argv[1]) as fp:
+        source = fp.read()
+    print(source)
+    lexer.input(source)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break      # No more input
+        print(tok.type, tok.value, tok.lineno, tok.lexpos)
