89ac7c8be5423bcdad4c09e26233939544491048
Jonas Treplin <jonas@treplin.de>
2023-09-28 16:20:41 +0200

    add bx_runtime

---
+++ starter/bx_runtime.c
@@ -0,0 +1,9 @@
+/* This should be in a file such as: bx_runtime.c */
+#include <stdio.h>
+#include <stdint.h>
+/* Note: TAC int == C int64_t
+This is because C int is usually only 32 bits. */
+void bx_print_int(int64_t x)
+{
+    printf("%ld\n", x);
+}
