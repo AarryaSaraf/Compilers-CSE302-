713dee5c57530e8063e08f28dd871fa09d007579
Jonas Treplin <jonas@treplin.de>
2023-10-03 11:06:48 +0200

    extend main to full compilation pipeline

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
+import os
 from lib.asmgen import AsmGen
 from lib.parser import parser
 from lib.checker import pp_errs, check_programm
 from lib.bmm import bmm
 from lib.tac import var_mapping
+
 if __name__ == "__main__":
     with open(sys.argv[1]) as fp:
         source = fp.read()
@@ -17,5 +19,18 @@ if __name__ == "__main__":
     tac = bmm(ast.stmts, vars_to_tmp)
 
     asm_gen = AsmGen(tac)
-
-    print(asm_gen.compile())
+    asm = asm_gen.compile()
+    if "--nolink" in sys.argv:
+        print(asm)
+    else:
+        if "-o" in sys.argv:
+            i = sys.argv.index("-o")
+            output = sys.argv[i+1]
+        else:
+            output = "out"
+        fp = open(f"{output}.S", "w")
+        fp.write(asm)
+        fp.close()
+        os.system(f"gcc -o {output}.o {output}.S bx_runtime.c")
+        
+    
