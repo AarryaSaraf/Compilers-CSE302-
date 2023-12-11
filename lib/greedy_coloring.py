a99d8b119be76e4e872c7d9f573659f62b81233b
AarryaSaraf <aarryasaraf@gmail.com>
2023-12-11 21:36:28 +0100

    organisation

--- lib/greedy_coloring.py
+++ lib/greedy_coloring.py
@@ -1,5 +1,8 @@
 import random
 from .mcs import *
+from .ssa import *
+from .tac import *
+
 
 color_map = (
 '%%rax', '%%rcx', '%%rdx', '%%rsi', '%%rdi', '%%r8', '%%r9', '%%r10',
@@ -94,7 +97,8 @@ def allocate(params, ret, G, elim):
     ret : temp
         temp where the return value of the proc is stored
     G : interferance graph
-
+    elim: list[temps]
+        elimination ordering
     Returns
     -------
     int, dict
@@ -103,6 +107,7 @@ def allocate(params, ret, G, elim):
     """
 
     col = greedy_coloring(params, ret, G, elim)
+    # the register coalsecing will mp go here
     to_spill = spill(col)
     spilled = []
     while to_spill is not None:
@@ -118,6 +123,53 @@ def allocate(params, ret, G, elim):
     return stacksize, alloc
 
 
+# generate colored graph 
+# then coalsecing
+# then allocation
+
+def rcol(G, C, cop):
+    """register coalsecing changes instructions or merges registers
+
+    Args:
+        G (InterferenceGraph): IFGraph
+        C (Dictionary tmp -> Int): Coloring
+        cop (list of lists): each inner list contains a boolean (true if copy false if not, %a, %b where the instruction is %b = copy %a)
+    """
+    for b in cop:
+        if(b[0] and C[b[1]] == C[b[2]]):
+            b = None
+        elif(b[1] not in G.nodes[b[2]].nbh):
+            flag  = exc(G, C,b)
+            if(flag):
+                flag -=1
+                tmp = TACTemp(flag, 0)
+                G = adder(G, tmp, b[1], b[2])
+                G = remove(G, b[1])
+                G = remove(G, b[2])
+                # replace  %a and %b with %c in the instruction space JONAS TO DO 
+                
+def exc(G, C, b):
+    """ Check is such a c exists
+
+    Args:
+        G (InterfereneGraph): Interferene graph
+        C (Dictionary tmp -> Int): Coloring
+        b ([bool, %a, %b]): 1 element of cop from the preious function
+
+    Returns:
+        _type_: False if no such c exists
+                (c+1) if c exists. It is c+1 so it necessarily satisfies the if(c) conditional.
+    """
+    u = G.nodes[b[1]].nbh + G.nodes[b[2]].nbh
+    col = []
+    for i in u:
+        col.append(C[i])
+    flag = False
+    for i in list(C.values()):
+        if i not in col:
+            flag = (i+1)
+    return flag
+                
 # example from the lecture
 G = {'a': ['d'],
      'd': ['a', 'e', 'c', 'b'],
@@ -128,11 +180,33 @@ G = {'a': ['d'],
     
 elim = ['b', 'd', 'c', 'e', 'a']
 
+class GraphAndColor():
+    def __init__(self, lin, de, use, cop, ):
+        """ 
+        lin (list of list of SSATemps or TACTemps): live in
+        de (list of list of SSATemps or TACTemps)): def
+        use (list of list of SSATemps or TACTemps): use
+        cop (list of Bool): copy or not. List of booleans
+        
+        """
+        self.ig = transformer(lin,de,use,cop)
+        self.seo = mcs(self.ig)
+        
+    def allocated(self, params, ret):
+        """
 
+        Args:
+            params : list[temps]
+        list of temps which store the parameters of the function
+            ret : temp
+        temp where the return value of the proc is stored
 
+        Returns:
+            dict temps -> int: values are the assigned color (int) 
+        """
+        return allocate((params, ret, self.ig, self.seo) )
 
-
-
+        
 
 
 
